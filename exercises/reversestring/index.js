c8fcde3b70a45679468e7a1cfe29fbacf418fc1e
Ian Laue <ian.laue.268@my.csun.edu>
2018-08-27 12:31:35 -0700

    reversestring-done

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,61 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+
+    var reversed = '';
+
+    for (const char of str) {
+        reversed = char + reversed;
+    }
+
+    return reversed;
+}
 
 module.exports = reverse;
+
+
+
+/*
+Using JS string and array library
+*/
+// var reverse = str => str.split('').reverse().join('');
+
+
+/*
+Using Forin syntax 
+*/
+// function reverse(str) {
+
+//     var reversed = '';
+
+//     for (const char of str) {
+//         reversed = char + reversed;
+//     }
+
+//     return reversed;
+// }
+
+
+/*
+Using Classic For Loop
+*/
+// function reverse(str) {
+
+//     var reversed = '';
+
+//     for (let index = str.length - 1; index >= 0; index--) {
+//         reversed += str[index];
+//     }
+
+//     return reversed;
+// }
+
+
+/*
+Using Advanced arrray helper function
+*/
+// function reverse(str) {
+
+//     return str.split('').reduce((rev, char) => char + rev, '');
+// }
