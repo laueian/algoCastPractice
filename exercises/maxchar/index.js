2750e8298573a889a7e1a2be40bb6414706eb905
Ian Laue <ian.laue.268@my.csun.edu>
2018-08-27 14:04:41 -0700

    maxchar-done

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,24 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+
+    const charMap = {};
+    let maxCount = 0;
+    let maxChar = '';
+
+    for (const char of str) {
+        charMap[char] = charMap[char] + 1 || 1;
+    }
+
+    for (let char in charMap) {
+        if (charMap[char] > maxCount) {
+            maxCount = charMap[char];
+            maxChar = char;
+        }
+    }
+
+    return maxChar;
+}
 
 module.exports = maxChar;
