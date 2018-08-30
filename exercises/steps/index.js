197802640775c69dffe4df4c4ddd188ea5185aaf
Ian Laue <ian.laue.268@my.csun.edu>
2018-08-29 17:04:47 -0700

    steps-done

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,38 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n, row = 0, stair = '') {
+
+    if (n === row)
+        return
+
+    if (n === stair.length) {
+        console.log(stair)
+        return steps(n, row + 1);
+    }
+
+    const add = stair.length <= row ? '#' : ' '
+    // if (stair.length <= row)
+    //     stair += '#'
+    // else
+    //     stair += ' '
+
+    steps(n, row, stair + add)
+}
 
 module.exports = steps;
+
+
+// function steps(n) {
+
+//     for (let row = 0; row < n; row++) {
+//         let stair = '';
+//         for (let columun = 0; columun < n; columun++) {
+//             if (columun <= row)
+//                 stair += '#'
+//             else
+//                 stair += ' '
+//         }
+//         console.log(stair)
+//     }
+// }
